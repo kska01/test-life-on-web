96c948a6a0f9cf5c7a790f46b602f3681cff018f
kska01 <insideka01@gmail.com>
2025-05-26 23:19:17 +0900

    feat: 파일 업로드 시 예외처리를 위한 FileUploadException 추가

---
+++ be/src/main/java/com/lifeon/test/global/exception/FileUploadException.java
@@ -0,0 +1,12 @@
+package com.lifeon.test.global.exception;
+
+public class FileUploadException extends RuntimeException {
+
+    public FileUploadException(String message) {
+        super(message);
+    }
+
+    public FileUploadException() {
+        super("파일 업로드 실패.");
+    }
+}
