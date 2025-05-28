6796f038d84adbea9f49ae2c5857978f9b7a81a9
kska01 <insideka01@gmail.com>
2025-05-28 20:28:44 +0900

    fix: Header의 이미지와 Footer의 string을 반응형으로 변경, 로고 이미지 이름 변경

--- fe/src/components/Footer.jsx
+++ fe/src/components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Footer() {
-  return <p className='mb-25 text-2xl text-center'>©Life on Korea Inc. All rights reserved</p>;
+  return <p className='mb-25 text-[18px] md:text-xl lg:text-2xl xl:text-2xl text-center'>©Life on Korea Inc. All rights reserved</p>;
 }
--- fe/src/components/Header.jsx
+++ fe/src/components/Header.jsx
@@ -5,7 +5,7 @@ export default function Header() {
   return (
     <>
       <Link to="/">
-        <img src="src/assets/lifeon-logo.jpg" alt="lifeOn logo" className="pt-[2rem] px-[2rem]" />
+        <img src="src/assets/life-on-logo.jpg" alt="lifeOn logo" className="pt-[2rem] px-[2rem] w-65 md:w-75 lg:w-85 xl:w-100" />
       </Link>
     </>
   );
