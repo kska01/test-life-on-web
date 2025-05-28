e9bb04435eb387422cfaf88dffe6205f0012b0b9
kska01 <insideka01@gmail.com>
2025-05-29 00:55:14 +0900

    feat: axios 설치 및 인스턴스와 fileUploadApi 정의

---
+++ fe/src/api/fileUploadApi.js
@@ -0,0 +1,16 @@
+import api from './axios';
+
+const ENDPOINT = '/plan-files';
+const fileUploadApi = {
+  
+  uploadFile: async (formData) => {
+    const response = await api.post(`${ENDPOINT}`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+    return response.data;
+  },
+};
+
+export default uploadFileApi;
