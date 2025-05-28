5358bed7ea7e460a4c59265d7afd29a8e803b17a
kska01 <insideka01@gmail.com>
2025-05-28 18:01:59 +0900

    feat: Home 화면 구현

--- fe/src/pages/Home.jsx
+++ fe/src/pages/Home.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function Home() {
-  return <h1>turn on your life</h1>;
+  const navigate = useNavigate();
+
+  const handleButtonClick = (e) => {
+    console.log('click');
+    navigate('/upload');
+  };
+
+  return (
+      <main className="flex flex-col flex-grow gap-30 mt-50 items-center text-center">
+        <h1 className="text-6xl">turn on your office life</h1>
+        <button onClick={handleButtonClick} 
+        className="text-4xl underline underline-offset-8 hover:text-yellow-500 p-2 cursor-pointer">
+          upload file
+        </button>
+      </main>
+  );
 }
