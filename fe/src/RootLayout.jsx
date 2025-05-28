7183c1af0f59e34928b8f67cafbabbdee419ebf8
kska01 <insideka01@gmail.com>
2025-05-28 18:00:09 +0900

    design: 아이템들의 화면배치를 위한 설정 추가

--- fe/src/RootLayout.jsx
+++ fe/src/RootLayout.jsx
@@ -5,10 +5,10 @@ import { Outlet } from "react-router-dom";
 
 export default function RootLayout() {
   return (
-    <>
+    <section className="flex flex-col min-h-screen">
       <Header></Header>
       <Outlet></Outlet>
       <Footer></Footer>
-    </>
+    </section>
   );
 }
