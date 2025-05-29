c27f872c196bc31c3e99396ef87323a4686ff242
kska01 <insideka01@gmail.com>
2025-05-29 15:58:54 +0900

    feat: 파일 로딩중 이미지 나타내기 기능 구현

--- fe/src/pages/Upload.jsx
+++ fe/src/pages/Upload.jsx
@@ -4,14 +4,16 @@ import fileUploadApi from '../api/fileUploadApi';
 
 export default function Upload() {
   const [uploadFile, setUploadFile] = useState(null);
+  const [isLoading, setIsLoading] = useState(null);
+  const [result, setResult] = useState(null);
 
   const handleFileSelect = (selectedFile) => {
     setUploadFile(selectedFile);
-    console.log('업로드된 파일:', uploadFile);
   };
 
   useEffect(() => {
     if (uploadFile) {
+      console.log('업로드된 파일:', uploadFile);
       handleUpload();
     }
   }, [uploadFile]);
@@ -19,19 +21,31 @@ export default function Upload() {
   const handleUpload = async () => {
     const formData = new FormData();
     formData.append('planFile', uploadFile);
+    setIsLoading(true);
 
     try {
       const response = await fileUploadApi.uploadFile(formData);
       console.log(response.data);
     } catch (error) {
       console.log(error);
-      
+    } finally {
+      setIsLoading(false);
     }
   };
 
   return (
     <>
-      <InputFile onChangeFile={handleFileSelect}></InputFile>
+      {isLoading ? (
+        <section className="flex flex-col flex-grow items-center mx-20 md:mx-50 lg:mx-80 xl:mx-95 mt-50">
+          <img className="h-64 w-auto" src="/src/assets/image_68.webp" alt="loading"></img>
+          <p>공간을 분석하고 있어요</p>
+          <p className="text-[14px] text-gray-300 mt-9">
+            업로드된 파일이름 : {uploadFile.name}
+          </p>
+        </section>
+      ) : (
+        <InputFile onChangeFile={handleFileSelect}></InputFile>
+      )}
     </>
   );
 }
